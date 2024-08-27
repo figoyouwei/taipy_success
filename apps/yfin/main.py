6aa52c43b228057c4804387bd0636b5de172eac8
Youwei Zheng <figoyouwei@gmail.com>
2024-08-27 16:23:42 +0800

    Issue 7: table add_row() works

--- apps/yfin/main.py
+++ apps/yfin/main.py
@@ -1,36 +1,24 @@
-'''
+"""
 @author: Youwei Zheng
 @target: Main app
-@update: 2024.08.13
-'''
-
-from datetime import datetime
+@update: 2024.08.27
+"""
 
 from taipy.gui import Gui
 
 import pages.calculator as page_calculator
 import pages.chatbot as page_chatbot
-import pages.yfin as page_yfin
+from pages.yfin import page_yfin, add_row
 
 from turing import compute_points
 from models.calculator import Level, Point
 
-from tools import download_yfin, process_yfin
+# ------------------------------
+# Main app
+# ------------------------------
 
 if __name__ == "__main__":
 
-    # Create yfinance page
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    args_in = (
-        '^SPX',
-        '1d',
-        '2024-08-01',
-        current_date,
-        )
-    df = download_yfin(args_in)
-    df_pcs = process_yfin(df)
-    page_yfin = page_yfin.create_page(df_pcs)
-   
     # Create calcuator page
     page_calculator = page_calculator.create_page()
 
@@ -45,9 +33,9 @@ if __name__ == "__main__":
     pages = {
         "/": "<center><|navbar|></center>",
         "yfinance": page_yfin,
-        "calculator": page_calculator, 
-        "chatbot": page_chatbot
-        }
+        "calculator": page_calculator,
+        "chatbot": page_chatbot,
+    }
 
     # Run page
     Gui(pages=pages).run(
@@ -57,6 +45,6 @@ if __name__ == "__main__":
         watermark="Made by CR7",
         margin="4em",
         host="0.0.0.0",
-        port=9000
+        port=9000,
         # base_url="/shggzyapp/" # key to nginx
     )
--- apps/yfin/pages/yfin.py
+++ apps/yfin/pages/yfin.py
@@ -1,12 +1,34 @@
-'''
+"""
 @author: Youwei Zheng
 @target: yfinance table page
 @update: 2024.08.26
-'''
+"""
+
+from datetime import datetime
 
 from taipy.gui import notify
 import taipy.gui.builder as tgb
 import pandas as pd
+
+from tools import download_yfin, process_yfin
+
+# ------------------------------
+# on_init
+# ------------------------------
+
+current_date = datetime.now().strftime("%Y-%m-%d")
+args_in = (
+    "^SPX",
+    "1d",
+    "2024-08-01",
+    current_date,
+)
+df = download_yfin(args_in)
+df_pcs = process_yfin(df)
+# data_df is used for your table; this is your bound variable
+# it is defined in the main.py so this variable is a global variable
+# you could or should define it in yfin.py
+data_df = df_pcs
 
 # ------------------------------
 # on_add
@@ -16,29 +38,27 @@ def add_row(state):
     empty_row = pd.DataFrame(
         [[None for _ in state.df_pcs.columns]], columns=state.df_pcs.columns
     )
-    state.df_pcs = pd.concat([empty_row, state.df_pcs], axis=0, ignore_index=True)
+    state.data_df = pd.concat([empty_row, state.df_pcs], axis=0, ignore_index=True)
 
     notify(state, "S", f"Added one row")
+
+def edit_cell(state):
+    pass
 
 # ------------------------------
 # Create page
 # ------------------------------
 
-def create_page(data_df=None) -> pd.DataFrame:
-    """Integrate pages and data.
+with tgb.Page() as page_yfin:
+    # Create title
+    tgb.toggle(theme=True)
+    tgb.text("# Table Data from yfinance ", mode="md", class_name="text-center pb1")
 
-    Returns:
-        _type_: _description_
-    """
-    with tgb.Page() as page:
-        # Create title
-        tgb.toggle(theme=True)
-        tgb.text("# Table Data from yfinance ", mode="md", class_name="text-center pb1")
-
-        # Create table
-        tgb.table(
-            "{data_df}",
-            on_add=add_row
-        )    
-
-    return page
+    # Create table
+    tgb.table(
+        "{data_df}", 
+        editable=True, 
+        on_add=add_row,
+        on_edit=edit_cell,
+        on_delete=False
+    )
