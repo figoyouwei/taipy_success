5c5dee454d67ed93d859cdb2686dcb4ac0009258
Youwei Zheng <figoyouwei@gmail.com>
2024-09-03 15:16:02 +0800

    Reply to Issue #10

--- chatbot/chat_calculator.py
+++ chatbot/chat_calculator.py
@@ -1,22 +1,8 @@
-# Copyright 2021-2024 Avaiga Private Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-# -----------------------------------------------------------------------------------------
-# To execute this script, make sure that the taipy-gui package is installed in your
-# Python environment and run:
-#     python <script>
-# -----------------------------------------------------------------------------------------
-# Human-computer dialog UI based on the chat control.
-# -----------------------------------------------------------------------------------------
-
-from math import cos, pi, sin, sqrt, tan  # noqa: F401
+"""
+@author: Youwei Zheng
+@target: Issue #10
+@update: 2024.09.03
+"""
 
 import taipy.gui.builder as tgb
 from taipy.gui import Gui
