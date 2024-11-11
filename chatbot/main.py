6d72070b29d742b5419f844f59c579b9e44cef68
Youwei Zheng <figoyouwei@gmail.com>
2024-11-11 10:13:15 +0800

    Issue 33: toggle structure is ok

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,10 +1,11 @@
 """
 @author: Youwei Zheng
-@target: Solved icon-display issue with Taipy v4.0.0
-@update: 2024.10.21
+@target: Add partial sidebar
+@update: 2024.11.11
 """
 
 from dotenv import find_dotenv, load_dotenv
+
 load_dotenv(find_dotenv())
 
 from taipy.gui import Gui
@@ -15,18 +16,23 @@ from pages.chat import page_chat
 # Main app
 # ------------------------------
 
+from pages.home import toggle_partial_sidebar
+
+
+def on_init(state):
+    toggle_partial_sidebar(state)
+
+
 if __name__ == "__main__":
     print("Starting chatting with complete app...")
 
-    gui = Gui(
-        page=page_home,
-        css_file="./main.css"
-        )
+    gui = Gui(page=page_home, css_file="./main.css")
     partial_chat = gui.add_partial(page_chat)
+    partial_sidebar = gui.add_partial("")
 
     gui.run(
         dark_mode=True,
         title="Chat Demo v4.0",
         watermark="Shanghai Exchange Group",
-        use_reloader=True
-        )
+        use_reloader=True,
+    )
--- chatbot/pages/home.py
+++ chatbot/pages/home.py
@@ -1,7 +1,7 @@
 """
 @author: Youwei Zheng
-@target: sidebar page
-@update: 2024.10.31
+@target: sidebar page with toggle
+@update: 2024.11.11
 """
 
 import taipy.gui.builder as tgb
@@ -26,41 +26,60 @@ from pages.chat import sessions
 # Create page
 # ------------------------------
 
+# NOTE: display sidebar by default
+sidebar_switch = False
+
+def toggle_partial_sidebar(state):
+    if state.sidebar_switch:
+        with tgb.Page() as sidebar:
+            tgb.toggle("{sidebar_switch}", on_change=toggle_partial_sidebar)
+            tgb.part(class_name="p2 align-item-bottom table", partial="{partial_chat}")
+    else:
+        with tgb.Page() as sidebar:
+            with tgb.layout(columns="1 3", columns__mobile="1"):
+                # NOTE: sidebar class
+                with tgb.part(class_name="sidebar"):
+                    tgb.toggle("{sidebar_switch}", on_change=toggle_partial_sidebar)
+                    tgb.image(content="icons/icon_hm.png", class_name="profile_image")
+                    # sidebar titles
+                    tgb.text(
+                        "## Username", class_name="text-center profile_name", mode="md"
+                        )
+                    tgb.text(
+                        "## Verified Name", class_name="text-center", mode="md"
+                        )
+
+                    tgb.selector(
+                        value="{selected_session}",
+                        lov="{sessions}",
+                        on_change=select_session,
+                        # NOTE: displayed text of selector item
+                        type=ChatSession,
+                        adapter=selector_adapter,
+                        # NOTE: css identifier
+                        id="past_prompts_list",
+                        class_name="past_prompts_list",
+                    )
+
+                    # NOTE: reset part
+                    tgb.button(
+                        "New Session",
+                        on_action=reset_session,
+                        class_name="fullwidth plain",
+                    )
+
+                    # NOTE: selector part
+                    tgb.text(
+                        "### Previous activities",
+                        mode="md",
+                        class_name="h5 mt2 mb-half text-center",
+                    )
+                tgb.part(
+                    class_name="p2 align-item-bottom table", partial="{partial_chat}"
+                )
+
+    state.partial_sidebar.update_content(state, sidebar)
+
+
 with tgb.Page() as page_home:
-    # NOTE: fixed width 300px or 1 3
-    # with tgb.layout(columns="300px 1", columns__mobile="1"):
-    with tgb.layout(columns="1 3", columns__mobile="1"):
-        # NOTE: sidebar class
-        with tgb.part(class_name="sidebar"):
-            tgb.image(content="icons/icon_hm.png", class_name="profile_image")
-            tgb.text("## Human", class_name="text-center profile_name", mode="md")
-            tgb.selector(
-                value="{selected_session}",
-                lov="{sessions}",
-                on_change=select_session,
-                # NOTE: displayed text of selector item
-                type=ChatSession,
-                adapter=selector_adapter,
-                # NOTE: css identifier
-                id="past_prompts_list",
-                class_name="past_prompts_list",
-            )
-            # sidebar title
-            tgb.text("## Chatbot Demo", class_name="text-center", mode="md")
-
-            # NOTE: reset part
-            tgb.button(
-                "New Session",
-                on_action=reset_session,
-                class_name="fullwidth plain",
-            )
-
-            # NOTE: selector part
-            tgb.text(
-                "### Previous activities",
-                mode="md",
-                class_name="h5 mt2 mb-half text-center",
-            )
-
-        # NOTE: partial chat
-        tgb.part(class_name="p2 align-item-bottom table", partial="{partial_chat}")
+    tgb.part(partial="{partial_sidebar}")
